404c6a1606a6658ccb654d220d48c3c15a447c8e
tony1793 <antonio.ercolani@mail.polimi.it>
2022-07-01 16:55:44 +0200

    Update Environment.py

--- project/src/Environment.py
+++ project/src/Environment.py
@@ -2,29 +2,23 @@ import numpy as np
 
 class Environment:
 
-    def __init__(self, prices, prob_matrix, alphas, feature_1_dist, feature_2_dist):
+    def __init__(self, prices, prob_matrix, alphas, feature_1_dist, feature_2_dist, conversion_rates, primary_to_secondary_mapping, n_items_to_buy_distr):
 
-        self.prices = prices #the four prices for each product, they could be fixed for whole project?
-        #check if the prices are in increasing order? è scritto nella consegna
-
-        self.items = np.arange(0,5,1) #the five items, fare da 1 a 5 come dice nella consegna?
-
-        self.prob_matrix = prob_matrix #the graph probabilities, 3x5x5 matrix (3x since there will be one graph for each class of users)
-        #self.margins = margins?? The margins associated to the prices are known but do we have to consider them? higher price -> higher margin?
-
+        self.prices = prices #the four prices for each product 5x4
+        self.items = np.arange(0,5,1) #the five items
+        self.prob_matrix = prob_matrix #the probability matrices 5x5x3
+        #self.margins i margini per ogni prodotto vanno considerati?????
         self.alphas = alphas #3x6 (3 class of users -> 3 sets of alpha)
+        self.feature_1_dist = feature_1_dist #feature 1 distribution
+        self.feature_2_dist = feature_2_dist #feature 2 distribution
+        self.conversion_rates = conversion_rates #5x4x3
+        self.primary_to_secondary_mapping = primary_to_secondary_mapping #specify which secondary are showed after each primary
+        self.n_items_to_buy_distr = n_items_to_buy_distr
 
-        self.feature_1_dist = feature_1_dist #0.4
-        self.feature_2_dist = feature_2_dist #0.8
-
-        self.reservation_prices #????
-        #devono essere fissi per ogni classe di utenti? o tipo delle medie con deviazione
-        #da cui per ogni singolo utente ricavarsi il reservation price???????
 
 
     '''Computes the mapping between the user feature and the 
     corresponding class index. 
-    Useful for access the right probabilities ecc...
     feature_1 == 0 and feature_2 == 0 -> class 0
     feature_1 == 1 and feature_2 == 1 -> class 1
     (feature_1 == 1 and feature_2 == 0) OR (feature_1 == 0 and feature_2) == 1 -> class 2 
@@ -37,95 +31,31 @@ class Environment:
         else:
             return 2
 
-    #il corrispondente del metodo 'round'
-    def purchase(self, item, price, user_class, reservation_price):
-        #controlla se il reservation price dell'utente supera il prezzo dell'item
-        if price < reservation_price:
-            yes_or_no = np.random.binomial(1, self.probabilities[user_class][price])
+    #equivalent of the round method
+    def purchase(self, item, price, user_class):
+        purchase_outcome = np.random.binomial(1, self.conversion_rates[user_class][item][price])
+        return 1 #1 or 0 = the user buys or not
 
-    #qui immagino che ci debba essere un mapping fisso
-    #1:2 con ad ogni primary una coppia di secondary da spawnare
-    def get_secondary(self, primary):
-        #es se il primary è l'item 0 ritorno i secondary 2 e 4
-        #ovviamente anche l'ordine è importante per la questione dell'ordine di visualizzazione
-        if primary == 0:
-            return np.array([2,4])
-
-    def click_on_secondary(self, user_class, bought_items):
-        #usando le probabilità del grafo ci dice
-        # la prossima mossa dell'utente (click su uno dei due secodnary o uscita direi)
-        #l'uscita deve restituire -1 per l'uscita dal ciclo
-        return 999
+    #retrieve the secondary items clicked by the user
+    def get_clicked_secondary(self, user_class, bought_items, primary):
+        secondary_prob = self.prob_matrix[user_class][primary]
+        clicked_secondary = []
+        for secondary in self.primary_to_secondary_mapping[primary]:
+            if bought_items[secondary] == 0:
+                click_outcome = np.random.binomial(1, secondary_prob[secondary])
+                if click_outcome == 1:
+                    clicked_secondary.append(secondary)
+        return clicked_secondary
 
 
+    def get_items_sold(self, item, user_class):
+        mu, sigma = self.n_items_to_buy_distr[user_class, item]
+        items_sold = int(np.round(np.random.normal(mu, sigma, 1)))
+        if items_sold == 0.0:
+            items_sold = 1
+        return items_sold
 
 
-
-
-class Simulator:
-
-    def __init__(self, days, users):
-        self.days = days
-        self.users = users #quanti giri fare dentro al grafo ogni giorno
-        self.e = Environment(...)
-
-        #self.badit del pricing
-        #self.algoritmo per la social influence
-
-    def run_simulation(self):
-
-        for day in self.days:
-            #cambiare prezzi in base ai bandit
-            prices = 'array di prezzi'
-
-            for user in self.users:
-                #retrieve the user features -> user class
-                feature_1 = np.random.choice([0,1],p=[1-self.e.feature_1_dist, self.e.feature_1_dist])
-                feature_2 = np.random.choice([0,1],p=[1-self.e.feature_2_dist, self.e.feature_2_dist])
-                user_class = self.e.user_class_mapping(feature_1, feature_2)
-
-                #item di partenza usando gli alpha
-                webpages = np.concatenate((np.array([-1]), self.e.items), axis=0)
-                starting_point = np.random.choice(webpages, p=self.e.alphas[user_class])
-
-                #maschera per salvarci quali items abbiamo già comprato
-                bought_items = np.zeros(self.e.items.shape[0])
-
-                #se l'utente non è andato dalla concorrenza
-                if starting_point != -1:
-
-                    primary = starting_point
-
-                    #mi sembra sensato mettere il primary a -1 quando l'utente vuole uscire così da fermare il ciclo
-                    while primary != -1:
-                        #modellare con una coda con piazzamenti a random in testa
-                        #per il discorso dei path multipli
-
-                        #ci interessa solo se l'utente compra o anche quanti oggetti compra?
-                        if self.e.purchase(primary, prices[user_class][primary], user_class):
-
-                            bought_items[primary] = 1
-
-                            #salvarsi dati per bandit del pricing
-                            #per la social influence è imporatante salvarsi anche se l'utente ha comprato
-                            #un certo oggetto?
-
-                            secondary = self.e.get_secondary(primary)
-
-                            primary = self.e.click_on_secondary(user_class, bought_items)
-
-                            #salvarsi i dati per l'algoritmo della social influence
-
-                            #TENERE CONTO DEI PATH MULTIPLI
-
-                        else:
-                            #l'utente non ha comprato quindi esce dal ciclo
-                            #dobbiamo salvarci qualche dato per l'aggiornamento a fine giornata?
-                            primary = -1
-
-            #GIORNATA FINITA
-            #aggiornare social influence
-            #aggiornare algoritmo pricing
 
 
 

bb9c504bb0e93ceeb5a954af1ac0ff30fb0d37cc
tony1793 <antonio.ercolani@mail.polimi.it>
2022-07-07 11:26:57 +0200

    Update UCB_Learner.py

--- project/src/UCB_Learner.py
+++ project/src/UCB_Learner.py
@@ -1,23 +1,17 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from Learner import *
 
 class UCB_Learner(Learner):
     def __init__(self, prices):
-        super().__init__(prices.shape)
-        
-        #5x4
+        super().__init__(prices)
         self.prices = prices
-
-        #5x4
         self.means = np.zeros(prices.shape)
-        #5x4
         self.widths = np.ones(prices.shape) * np.inf
         
     
     def pull_prices(self): #to pick the 5 arms, 1 for each item, return the indexes inside prices matrix
         idx = np.zeros(self.n_items, dtype=int)
-       
+       #todo considerare anche il numero di item venduti per ogni item
         for i in range(self.n_items):
             idx[i] = np.argmax((self.means[i, :]+self.widths[i, :])*self.prices[i, :])
         return idx
@@ -28,7 +22,8 @@ class UCB_Learner(Learner):
 
         # i mi dice qual è l'item, pulled_arm[i] mi dice l'arm pullato per l'item i
         for i in range(self.n_items):
-            self.means[i][pulled_arms[i]] = np.mean(self.rewards_per_item_arm[i][pulled_arms[i]])
+            if len(rewards[i]) != 0:
+                self.means[i][pulled_arms[i]] = np.mean(self.rewards_per_item_arm[i][pulled_arms[i]])
         
         for idx in range(self.n_items):
             for idy in range(self.n_prices):

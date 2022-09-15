83d6cbff9c22b29ce72ef23a157cfd666fdf5107
Antonio Ercolani <antonio.ercolani@mail.polimi.it>
2022-09-15 16:00:43 +0200

    Update simulator_tester_step6.py

--- project/src/simulator_tester_step6.py
+++ project/src/simulator_tester_step6.py
@@ -15,11 +15,6 @@ from Number_of_sold_items_estimator import Number_of_sold_items_estimator
 seed = 15
 np.random.seed(seed)
 
-# prices = np.array([[0.1, 0.2, 0.3, 0.4],
-#                    [0.1, 0.2, 0.3, 0.4],
-#                    [0.1, 0.2, 0.3, 0.4],
-#                    [0.1, 0.2, 0.3, 0.4],
-#                    [0.1, 0.2, 0.3, 0.4]])
 
 prices = np.array([[1, 2, 3, 4],
                    [1, 2, 3, 4],
